f0a14488dd11b56ff8e92b04c0265ee9dfbb537f
Tammer Saleh <tsaleh@gmail.com>
2009-05-31 11:19:13 -0700

    Page 89

--- Control Fun/Classes/Control_FunViewController.h
+++ Control Fun/Classes/Control_FunViewController.h
@@ -7,21 +7,32 @@
 //
 
 #import <UIKit/UIKit.h>
+#define kShowSegmentIndex 0
 
-
-@interface Control_FunViewController : UIViewController {
+@interface Control_FunViewController : UIViewController <UIActionSheetDelegate> {
 	IBOutlet UITextField *nameField;
 	IBOutlet UITextField *numberField;
 	IBOutlet UILabel     *sliderLabel;
+	IBOutlet UISwitch    *leftSwitch;
+	IBOutlet UISwitch    *rightSwitch;
+	IBOutlet UIView      *switchView;
+	IBOutlet UIButton    *doSomethingButton;
 }
 
 @property (nonatomic, retain) UITextField *nameField;
 @property (nonatomic, retain) UITextField *numberField;
 @property (nonatomic, retain) UILabel     *sliderLabel;
+@property (nonatomic, retain) UISwitch    *leftSwitch;
+@property (nonatomic, retain) UISwitch    *rightSwitch;
+@property (nonatomic, retain) UIView      *switchView;
+@property (nonatomic, retain) UIButton    *doSomethingButton;
 
 - (IBAction)textFieldDoneEditing:(id)sender;
 - (IBAction)backgroundClick:(id)sender;
 - (IBAction)sliderChanged:(id)sender;
+- (IBAction)switchChanged:(id)sender;
+- (IBAction)toggleShowHide:(id)sender;
+- (IBAction)doSomething:(id)sender;
 
 @end
 
--- Control Fun/Classes/Control_FunViewController.m
+++ Control Fun/Classes/Control_FunViewController.m
@@ -12,6 +12,65 @@
 @synthesize nameField;
 @synthesize numberField;
 @synthesize sliderLabel;
+@synthesize leftSwitch;
+@synthesize rightSwitch;
+@synthesize switchView;
+@synthesize doSomethingButton;
+
+- (void)viewDidLoad {
+	UIImage *buttonImageNormal = [UIImage imageNamed:@"whitebutton.png"];
+	UIImage *stretchableButtonImageNormal = [buttonImageNormal stretchableImageWithLeftCapWidth:12 topCapHeight:0];
+	[doSomethingButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
+	
+	UIImage *buttonImagePressed = [UIImage imageNamed:@"blackbutton.png"];
+	UIImage *stretchableButtonImagePressed = [buttonImagePressed stretchableImageWithLeftCapWidth:12 topCapHeight:0];
+	[doSomethingButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
+}
+
+- (IBAction)doSomething:(id)sender {
+	UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Are you sure?" 
+															 delegate:self 
+													cancelButtonTitle:@"No way!" 
+											   destructiveButtonTitle:@"Yes, I'm sure." 
+													otherButtonTitles:nil];
+	[actionSheet showInView:self.view];
+	[actionSheet release];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex {
+	if (!(buttonIndex == [actionSheet cancelButtonIndex])) {
+		NSString *msg = nil;
+		
+		if (nameField.text.length > 0)
+			msg = [[NSString alloc] initWithFormat:@"You can breath easy, %@, everything went OK.", nameField.text];
+		else
+			msg = @"You can breath easy, everything went OK.";
+			
+		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Something was done" 
+														message:msg 
+													   delegate:nil 
+											  cancelButtonTitle:@"Phew!" 
+											  otherButtonTitles:nil];
+		[alert show];
+		[alert release];
+		[msg release];
+	}
+}
+
+- (IBAction)switchChanged:(id)sender {
+	UISwitch *whichSwitch = (UISwitch *)sender;
+	BOOL setting = whichSwitch.isOn;
+	[leftSwitch setOn:setting animated:YES];
+	[rightSwitch setOn:setting animated:YES];
+}
+
+- (IBAction)toggleShowHide:(id)sender {
+	UISegmentedControl *segmentedControl = (UISegmentedControl *)sender;
+	NSInteger segment = segmentedControl.selectedSegmentIndex;
+	
+	if (segment == kShowSegmentIndex) [switchView setHidden:NO];
+	else [switchView setHidden:YES];
+}
 
 - (IBAction)sliderChanged:(id)sender {
 	UISlider *slider = (UISlider *)sender;
